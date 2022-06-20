f11db10368c836fb403ae5f2f0ccb8795b8b6f38
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-20 12:18:05 +0200

    implement addUser and addRole methods in UserServiceImpl

--- src/main/java/com/user/management/service/UserServiceImpl.java
+++ src/main/java/com/user/management/service/UserServiceImpl.java
@@ -2,20 +2,31 @@ package com.user.management.service;
 
 import com.user.management.entities.Role;
 import com.user.management.entities.User;
+import com.user.management.repositories.RoleRepository;
+import com.user.management.repositories.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
 @Service
 @Transactional
+@AllArgsConstructor
 public class UserServiceImpl implements UserService{
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
     @Override
     public User addUser(User user) {
-        return null;
+        user.setUserId(UUID.randomUUID().toString());
+        return userRepository.save(user);
     }
 
     @Override
     public Role addRole(Role role) {
-        return null;
+        return roleRepository.save(role);
     }
 
     @Override
