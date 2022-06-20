58b32810d8cfb9bb5a5673504e0abd0a9c09a0e9
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-20 11:50:19 +0200

    RoleRepository created

---
+++ src/main/java/com/user/management/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.user.management.repositories;
+
+import com.user.management.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
